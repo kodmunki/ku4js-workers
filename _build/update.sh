6b7884e169952b21294ce4863c511d08c98c5429
kodmunki <kodmunki@gmail.com>
2015-02-28 13:27:23 -0600

    Update script

--- _build/update.sh
+++ _build/update.sh
@@ -6,14 +6,18 @@ cp -f ../bin/ku4js-workers.js ../../ku4js-workers/example/scripts/example/lib/
 cp -f ../bin/ku4js-workers-uncompressed.js ../../ku4js-workers/tests/_dependencies/
 
 cp -f ../bin/ku4js-workers.js ../../ku4js-webApp/tests/_dependencies/
-cp -f ../bin/ku4js-workers.js ../../ku4js-webApp/example/scripts/example/lib/
+cp -f ../bin/ku4js-workers.js ../../ku4js-webApp/_example/javascript/lib/
 cp -f ../bin/ku4js-workers.js ../../ku4js-webApp/_TEMPLATE/lib/
 
 cp -f ../docs/thread/ku4js-workers-thread.js ../bin/
 cp -f ../docs/thread/ku4js-workers-thread.js ../../ku4js-workers/tests/_dependencies/
 
 cp -f ../bin/ku4js-workers-thread.js ../../ku4js-webApp/tests/_dependencies/
-cp -f ../bin/ku4js-workers-thread.js ../../ku4js-webApp/example/scripts/example/lib/
+cp -f ../bin/ku4js-workers-thread.js ../../ku4js-webApp/_example/javascript/lib/
 cp -f ../bin/ku4js-workers-thread.js ../../ku4js-webApp/_TEMPLATE/lib/
+
+echo "Copying resources to _LATEST/"
+cp -f ../bin/ku4js-workers.js ../../_LATEST/
+cp -f ../bin/ku4js-workers-thread.js ../../_LATEST/
 
 echo "Update complete :{)}"
